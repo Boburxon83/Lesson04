a8f0e3e5cb07d1baaef3d49b05b965f079f8c3ce
Boburxon83 <137619319+Boburxon83@users.noreply.github.com>
2023-07-11 20:50:59 +0500

    Добавьте файлы проекта.

---
+++ Lesson04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lesson04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
